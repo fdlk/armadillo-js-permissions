f7ea75630caec82a9d144c63fd54a419beb372e1
Fleur Kelpin <f.kelpin@umcg.nl>
2020-09-11 22:23:08 +0200

    feat: register and unregister

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <div id="app">
     <application></application>
-    <h1>Add users</h1>
+    <h1>Find users</h1>
     <input type="text" v-model.lazy="searchQuery"/>
-    <ul>
-      <li v-for="user in users" :key="user.id">{{user.email}}
-        <ul>
-          <li v-for="role in user.roles" :key="role">{{role}}</li>
-        </ul>
-      </li>
-    </ul>
+    <table v-if="users">
+      <caption>Users</caption>
+      <thead>
+        <th id="email">Email</th>
+        <th id="name">Name</th>
+        <th id="roles">Roles</th>
+        <th id="register"></th>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{user.email}}</td>
+          <td>{{user.name || getNameFromParts(user)}}</td>
+          <td>{{user.roles.join(",") || "-"}}</td>
+          <td v-if="!user.registered"><button @click="register(user.id)">Register</button></td>
+        </tr>
+      </tbody>
+    </table>
     <h1>Registered users</h1>
-    <ul>
-      <li v-for="user in registeredUsers" :key="user.id">{{user.email}}
-        <ul>
-          <li v-for="role in user.roles" :key="role">{{role}}</li>
-        </ul>
-      </li>
-    </ul>
+    <table v-if="registeredUsers">
+      <caption>Users</caption>
+      <thead>
+        <th id="email">Email</th>
+        <th id="name">Name</th>
+        <th id="roles">Roles</th>
+        <th id="register"></th>
+      </thead>
+      <tbody>
+        <tr v-for="user in registeredUsers" :key="user.id">
+          <td>{{user.email}}</td>
+          <td>{{user.name || getNameFromParts(user)}}</td>
+          <td>{{user.roles.join(",") || "-"}}</td>
+          <td><button @click="unregister(user.id)">x</button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -30,6 +50,39 @@ export default {
   components: {
     Application
   },
+  methods: {
+    getNameFromParts (user) {
+      return [user.firstName, user.lastName].filter(it => !!it).join(' ')
+    },
+    async register (userId) {
+      await this.$apollo.mutate({
+        mutation: gql`mutation ($userId: String!) {
+          register(userId: $userId) {
+            id
+            roles
+            registered
+          }
+        }`,
+        variables: {
+          userId
+        }
+      })
+    },
+    async unregister (userId) {
+      await this.$apollo.mutate({
+        mutation: gql`mutation ($userId: String!) {
+          unregister(userId: $userId) {
+            id
+            roles
+            registered
+          }
+        }`,
+        variables: {
+          userId
+        }
+      })
+    }
+  },
   data () {
     return {
       searchQuery: 'Fleur'
@@ -41,6 +94,10 @@ export default {
         users(searchQuery: $searchQuery){
           id
           email
+          name
+          firstName
+          lastName
+          registered
           roles
         }
       }`,
@@ -54,6 +111,10 @@ export default {
       registeredUsers {
         id
         email
+        name
+        firstName
+        lastName
+        registered
         roles
       }
     }`
